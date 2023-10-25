f2ec3f421c9a703d011e236f9fbf535f9187d99a
Yuhan Zhao <yuhanzhao@student.mitt.ca>
2023-10-25 12:49:06 -0500

    Code Finished

--- DesignPatternAssessmentBase/Models/Ticket.cs
+++ DesignPatternAssessmentBase/Models/Ticket.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DesignPatternAssessmentBase.Models
 {
@@ -14,8 +9,12 @@ namespace DesignPatternAssessmentBase.Models
         medium,
         high
     }
-
-    public class Ticket
+    public enum RequestType
+    {
+        Information,
+        Change
+    }
+    public abstract class Ticket
     {
         [Key]
         public int Id { get; set; }
@@ -45,5 +44,206 @@ namespace DesignPatternAssessmentBase.Models
         [InverseProperty("WatchedTickets")]
         public virtual ICollection<ApplicationUser> TaskWatchers { get; set; } = new HashSet<ApplicationUser>();
         public virtual ICollection<Comment> Comment { get; set; } = new HashSet<Comment>();
+
+        public virtual int Assign {  get; set; }
+        public virtual int Resolve { get; set; }
+        protected IAssigne _assignBehaviour;
+        protected IResolve _resolveBehaviour;
+        public abstract Ticket GetType(object input);
+        public virtual void SetAssign(int assigned)
+        {
+            Assign = assigned;
+            _assignBehaviour.Assign(assigned);
+        }
+        public virtual int GetAssign()
+        {
+            return Assign;
+        }
+        public virtual void SetResolve(int resolved)
+        {
+            Resolve = resolved;
+            _resolveBehaviour.Resolve(resolved);
+        }
+        public virtual int GetResolve()
+        {
+            return Resolve;
+        }
+        public virtual void CalculateTime()
+        {
+            Console.WriteLine($"Assign left hour: {Assign}, Resolve left hour: {Resolve}."); 
+        }
+    }   
+    public abstract class TicketFactory
+    {
+        public Ticket makeTicket(object input)
+        {
+            Ticket ticket;
+            ticket = CreateTicket(input);
+            return ticket;
+        }
+        protected abstract Ticket CreateTicket(object input);
+    }
+    public class BugReportFactory : TicketFactory
+    {
+        protected override Ticket CreateTicket(object input)
+        {
+            Ticket ticket;
+            if (input is string)
+            {
+                ticket = new BugReport().GetType(input);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+            return ticket;
+        }
+    }
+    public class RequestFactory : TicketFactory
+    {
+        protected override Ticket CreateTicket(object input)
+        {
+            Ticket ticket;
+            if (input is RequestType)
+            {
+                ticket = new Request();
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+            return ticket;
+        }
+    }
+    public class BugReport : Ticket
+    {
+        public override Ticket GetType(object input)
+        {
+            Ticket ticket;
+            if (input is string && (string)input == "Error Codes")
+            {
+                ticket = new ErrorCodesBugReport();
+            }
+            else if (input is string && (string)input == "Error Logs")
+            {
+                ticket = new ErrorLogsBugReport();
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+            return ticket;
+        }
+        public BugReport()
+        {
+            _assignBehaviour = new SetAssignBehaviour();
+            _resolveBehaviour = new SetResolveBehaviour();
+        }
+    }
+    public class ErrorCodesBugReport : BugReport
+    {
+
+    }
+    public class ErrorLogsBugReport : BugReport
+    {
+
+    }
+    public class Request : Ticket
+    {
+        public override Ticket GetType(object input)
+        {
+            Ticket ticket;
+            if (input is RequestType && (RequestType)input == RequestType.Information)
+            {
+                ticket = new InformationRequest();
+            }
+            else if (input is RequestType && (RequestType)input == RequestType.Change)
+            {
+                ticket = new ChangeRequest();
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+            return ticket;
+        }
+        public Request()
+        {
+            _assignBehaviour = new SetAssignBehaviour();
+            _resolveBehaviour = new SetResolveBehaviour();
+        }
+    }
+    public class InformationRequest : Request
+    {
+
+    }
+    public class ChangeRequest : Request
+    {
+
+    }
+    public interface IAssigne
+    {
+        void Assign(int assign);
+    }
+    public interface IResolve
+    {
+        void Resolve(int resolve);
+    }
+    public class SetAssignBehaviour: IAssigne
+    {
+        public void Assign(int assign)
+        {
+            Console.WriteLine($"The ticket assign hours left is {assign}.");
+        }
+    }
+    public class SetResolveBehaviour : IResolve
+    {
+        public void Resolve(int resolve)
+        {
+            Console.WriteLine($"The ticket resolve hours left is {resolve}.");
+        }
+    }
+    public abstract class ModifyHourDecorator : Ticket
+    {
+        public Ticket Ticket { get; set; }
+        public abstract override void CalculateTime();
+        public ModifyHourDecorator(Ticket ticket)
+        {
+            Ticket = ticket;
+        }
+        public override Ticket GetType(object input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class TypeBugReport : ModifyHourDecorator
+    {
+        public TypeBugReport(Ticket ticket) : base(ticket) { }
+        public override void CalculateTime()
+        {
+            Assign = Ticket.GetAssign() * 2;
+            Resolve = Ticket.GetResolve() * 2;
+            Console.WriteLine($"Assign left hour: {Assign}, Resolve left hour: {Resolve}.");
+        }
+    }
+    public class WhiteGloveClient : ModifyHourDecorator
+    {
+        public WhiteGloveClient(Ticket ticket) : base(ticket) { }
+        public override void CalculateTime()
+        {
+            Assign = Ticket.GetAssign() * 3;
+            Resolve = Ticket.GetResolve() * 3;
+            Console.WriteLine($"Assign left hour: {Assign}, Resolve left hour: {Resolve}.");
+        }
+    }
+    public class BacklogReissue : ModifyHourDecorator
+    {
+        public BacklogReissue(Ticket ticket) : base(ticket) { }
+        public override void CalculateTime()
+        {
+            Assign = Ticket.GetAssign() + 100;
+            Resolve = Ticket.GetResolve() + 100;
+            Console.WriteLine($"Assign left hour: {Assign}, Resolve left hour: {Resolve}.");
+        }
     }
 }
--- DesignPatternAssessmentBase/Program.cs
+++ DesignPatternAssessmentBase/Program.cs
@@ -1,9 +1,30 @@
 ﻿/*
- This assessment is designed to gauge your ability to solve a problem and explain your solution, not necessarily your ability to code in any specific syntax. You will provide  a short demonstration of your planned approach to the problem. You may add as much code, pseudocode, or notes to your project as you wish to aid in explaining your solution. 
+ This assessment is designed to gauge your ability to solve a problem and explain your solution, 
+ not necessarily your ability to code in any specific syntax. You will provide  
+ a short demonstration of your planned approach to the problem. You may add as much code, 
+ pseudocode, or notes to your project as you wish to aid in explaining your solution. 
 
-Be as detailed as possible in your explanation; show where and what changes you will make, preferably with names and properties picked out already, and indicate why you are making these changes.
+ Be as detailed as possible in your explanation; show where and what changes you will make, 
+ preferably with names and properties picked out already, and indicate why you are making 
+ these changes.
 
-All required changes should be made in the Models that have been already provided, or in a new class. No code needs to be added or changed in Program.cs.
- */
+ All required changes should be made in the Models that have been already provided, or in a 
+ new class. No code needs to be added or changed in Program.cs.
+*/
+
+using DesignPatternAssessmentBase.Models;
 
 Console.WriteLine("Good luck!");
+TicketFactory bugReportFactory = new BugReportFactory();
+Ticket bugReport = bugReportFactory.makeTicket("Error Codes");
+TicketFactory requestFactory = new RequestFactory();
+Ticket request = requestFactory.makeTicket(RequestType.Information);
+bugReport.SetAssign(4);
+bugReport.SetResolve(16);
+ModifyHourDecorator bugReportWithModifiers = new TypeBugReport(bugReport);
+bugReportWithModifiers = new WhiteGloveClient(bugReportWithModifiers);
+bugReportWithModifiers.CalculateTime();
+request.SetAssign(5);
+request.SetResolve(10);
+ModifyHourDecorator requestWithModifiers = new BacklogReissue(request);
+requestWithModifiers.CalculateTime();
